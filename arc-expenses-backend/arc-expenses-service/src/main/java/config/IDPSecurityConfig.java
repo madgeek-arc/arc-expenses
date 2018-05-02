acf6ff532675c4f069b415fb27f18927bb5265dc
panagiotiskanakakis <pkanakakis@di.uoa.gr>
2018-05-02 15:22:49 +0300

    New user schema. New controllers. Login security configuration

--- arc-expenses-backend/arc-expenses-service/src/main/java/config/IDPSecurityConfig.java
+++ arc-expenses-backend/arc-expenses-service/src/main/java/config/IDPSecurityConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
@@ -15,12 +16,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     SAMLLogoutSuccessHandler samlLogoutSuccessHandler;
 
+    @Autowired
+    SAMLBasicFilter samlBasicFilter;
+
+    @Autowired
+    SAMLRedirectFilter samlRedirectFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-//        http.addFilterBefore(new SAMLBasicFilter(), BasicAuthenticationFilter.class);
-//        http.addFilterAfter(new SAMLRedirectFilter(),FilterSecurityInterceptor.class);
+        http
+                .antMatcher("/user/*")
+                .addFilterBefore(samlBasicFilter, BasicAuthenticationFilter.class)
+                .addFilterAfter(samlRedirectFilter,FilterSecurityInterceptor.class)
 //        http.logout().logoutSuccessHandler(samlLogoutSuccessHandler).permitAll();
     }
 }
