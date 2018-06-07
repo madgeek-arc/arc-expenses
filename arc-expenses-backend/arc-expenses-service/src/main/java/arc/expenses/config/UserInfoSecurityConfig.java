5f58b924e006b65dafc2d9f28c78705e8912c0a8
panagiotiskanakakis <pkanakakis@di.uoa.gr>
2018-06-07 16:45:51 +0300

    1. Property placeholder 2. Docker production files

--- arc-expenses-backend/arc-expenses-service/src/main/java/arc/expenses/config/UserInfoSecurityConfig.java
+++ arc-expenses-backend/arc-expenses-service/src/main/java/arc/expenses/config/UserInfoSecurityConfig.java
@@ -1,5 +1,7 @@
 package arc.expenses.config;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
