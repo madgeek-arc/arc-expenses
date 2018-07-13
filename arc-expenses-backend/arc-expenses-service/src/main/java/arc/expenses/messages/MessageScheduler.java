db7771c94463d218710699fac709a0553a39a925
Konstantinos Spyrou <spyroukon@gmail.com>
2018-07-13 10:03:32 +0300

    changes in messages

--- arc-expenses-backend/arc-expenses-service/src/main/java/arc/expenses/messages/MessageScheduler.java
+++ arc-expenses-backend/arc-expenses-service/src/main/java/arc/expenses/messages/MessageScheduler.java
@@ -7,7 +7,7 @@ import arc.expenses.service.UserServiceImpl;
 import eu.openminted.registry.core.domain.FacetFilter;
 import gr.athenarc.domain.Request;
 import gr.athenarc.domain.User;
-import gr.athenarc.domain.Organization;
+import joptsimple.internal.Strings;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +15,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Component
 @Configuration
 @EnableScheduling
 public class MessageScheduler  {
+
+    private static final Logger logger = Logger.getLogger(MessageScheduler.class);
 
     @Autowired
     private UserServiceImpl userService;
@@ -31,35 +36,40 @@ public class MessageScheduler  {
     @Autowired
     private JavaMailer javaMailer;
 
-    Logger logger = Logger.getLogger(MessageScheduler.class);
-
-    List<EmailMessage> queue = null;
+    @Autowired
+    private StageMessages stageMessages;
 
     @Scheduled(cron = "${messages.cronExpr:0 0 8 * * ?}")
 //    @Scheduled(cron = "* * * * * ?") // use this for debugging
     public void scheduledEmails() {
         logger.info("Sending scheduled emails");
-        queue = new ArrayList<>();
+        List<EmailMessage> mailList = new ArrayList<>();
         List<User> users = userService.getAll(new FacetFilter()).getResults();
 
         for (User user: users) {
-            if ( "true".equals(user.getReceiveEmails()) && "false".equals(user.getImmediateEmails()) ) {
+            if ( "true".equals(user.getReceiveEmails()) /*&& "false".equals(user.getImmediateEmails())*/ ) {
                 List<Request> user_requests = requestService.getPendingRequests(user.getEmail());
                 if (user_requests.size() > 0) {
                     String text = createDigest(user_requests);
-                    queue.add(new EmailMessage(user.getEmail(), "[ARC-Expenses] Daily Digest", text));
+                    mailList.add(new EmailMessage(user.getEmail(), "[ARC-ν.4485] Daily Digest", text));
                 }
             }
         }
-        queue.forEach(email -> javaMailer.sendEmail(email.getRecipient(), email.getSubject(), email.getText()));
+        mailList.forEach(email -> {
+            logger.info(email);
+            javaMailer.sendEmail(email.getRecipient(), email.getSubject(), email.getText());
+        });
     }
 
-    private String createDigest(List<Request> requests) {
-        String text = "Εκκρεμεί η διεκπεραίωση των παρακάτω αιτημάτων: ";
-        for (Request req: requests) {
-            text += "\n" + req.getId();
-        }
-        return text;
-    }
+    private String createDigest(@NotNull List<Request> requests) {
+        StringBuilder text = new StringBuilder("Αγαπητέ χρήστη,\nΕκκρεμεί η διεκπεραίωση των παρακάτω αιτημάτων:\n");
 
+        List<String> pending = requests
+                .stream()
+                .map(r -> stageMessages.getRequestInfo(r))
+                .collect(Collectors.toList());
+        text.append(Strings.join(pending,"\n\n\n"));
+
+        return text.toString();
+    }
 }
