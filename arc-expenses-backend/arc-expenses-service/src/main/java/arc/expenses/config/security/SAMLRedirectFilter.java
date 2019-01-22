3b77f3a9c9fda7d08552875b775a975077a2bfb4
pkanakakis <pkanakakis@di.uoa.gr>
2019-01-22 14:56:06 +0200

    Redirect url from uri on login

--- arc-expenses-backend/arc-expenses-service/src/main/java/arc/expenses/config/security/SAMLRedirectFilter.java
+++ arc-expenses-backend/arc-expenses-service/src/main/java/arc/expenses/config/security/SAMLRedirectFilter.java
@@ -1,10 +1,12 @@
 package arc.expenses.config.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,15 +15,15 @@ import java.io.IOException;
 public class SAMLRedirectFilter extends GenericFilterBean {
 
 
-    @Value("${redirect.url}")
-    private String redirect_url;
+
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.sendRedirect(redirect_url);
+        String redirect_url = request.getQueryString();
+        response.sendRedirect(redirect_url.split("=")[1]);
         return;
     }
 }
